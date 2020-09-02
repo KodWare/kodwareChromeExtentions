64e8c3c9f5b8038ca9d31241505d69b02e329a09
raifpy <50047969+raifpy@users.noreply.github.com>
2020-09-02 11:08:07 +0300

    @KodWare

---
+++ clickHide/content.js
@@ -0,0 +1,8 @@
+// @raifpy tarafından @KodWare için kodlanmıştır :D
+$("[href]").hover(function(){
+  $(this).hide();
+},function(){
+  console.log("xd :D");
+  $(this).show(500)
+})
+// ilk jquery deneyimlerim , teşekkürler w3
